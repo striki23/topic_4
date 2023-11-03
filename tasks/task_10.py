3e6453bb80d5619a8792ac7fea3af5ce4bb6cdf6
Dasha Demidova <striki06@mail.ru>
2023-11-03 15:06:01 +0300

    done

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -4,8 +4,13 @@ hundreds = int(number[0])
 tens = int(number[1])
 units = int(number[2])
 
-sum_digits = hundreds + tens + units
+# Еще вариант без использования индекса строк:
 
+# hundreds = int(number)//100
+# tens = (int(number) - int(str(hundreds)+'00'))//10
+# units = int(number) - int(str(hundreds) + str(tens) + '0')
+
+sum_digits = hundreds + tens + units
 product_digits = hundreds * tens * units
 
 print('Сумма цифр: ', sum_digits)
