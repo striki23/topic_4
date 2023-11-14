f30938d7a884e5fc6379f111fc4cc973f7fe63b6
Dasha Demidova <striki06@mail.ru>
2023-11-14 16:02:49 +0300

    upgrate

--- tasks/task_10.py
+++ tasks/task_10.py
@@ -1,19 +1,11 @@
-number = input('Введите положительное трехзначное число: ')
+number = int(input('Введите положительное трехзначное число: '))
 
-hundreds = int(number[0])
-tens = int(number[1])
-units = int(number[2])
+number_3 = number % 10
+number_2 = number // 10 % 10
+number_1 = number // 100
 
-# Еще вариант без использования индекса строк:
+sum_digits = number_1 + number_2 + number_3
+product_digits = number_1 * number_2 * number_3
 
-# hundreds = int(number)//100
-# tens = (int(number) - int(str(hundreds)+'00'))//10
-# units = int(number) - int(str(hundreds) + str(tens) + '0')
-
-sum_digits = hundreds + tens + units
-product_digits = hundreds * tens * units
-
-print('Сумма цифр: ', sum_digits)
-print('Произведение цифр: ', product_digits)
-
-# TODO: Переделать
+print('Сумма цифр:', sum_digits)
+print('Произведение цифр:', product_digits)
--- tasks/task_11.py
+++ tasks/task_11.py
@@ -1,13 +1,11 @@
-number = input('Введите четырехзначное число: ')
+number = int(input('Введите четырехзначное число: '))
 
-thousands = int(number[0])
-hundreds = int(number[1])
-tens = int(number[2])
-units = int(number[3])
+thousands = number // 1000
+hundreds = number // 100 % 10
+tens = number // 10 % 10
+units = number % 10
 
 print('Цифра в позиции тысяч: ', thousands)
 print('Цифра в позиции сотен: ', hundreds)
 print('Цифра в позиции десятков: ', tens)
 print('Цифра в позиции единиц: ', units)
-
-# TODO: Переделать
